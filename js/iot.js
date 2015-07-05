8fff698c4138d41617dd6e01dedfa3f5ad83f195
Leon Anavi <leon@anavi.org>
2015-07-06 01:25:02 +0300

    Handle buttons
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ js/iot.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  
+  $('#buttonEntrance').bind('click', function(event) {
+    console.log('entrance');
+  });
+
+  $('#buttonExit').bind('click', function(event) {
+    console.log('exit');
+  });
+
+});
