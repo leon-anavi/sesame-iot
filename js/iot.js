c6370dda752f6fb58b66d9a35be0984fa9c7aa7b
Leon Anavi <leon@anavi.org>
2015-07-22 03:09:59 +0300

    Show notification at the UI if acknowledgement that the door or the barrier has been opened succesfully

--- js/iot.js
+++ js/iot.js
@@ -1,6 +1,14 @@
 //Delay between opeing the 1st and the 2nd door (in seconds)
 var settingsDelay = 30;
 
+var mqttHost = 'iot.anavi.org';//location.hostname;
+var mqttPort = 80;//location.port;
+var mqttPath = '/mosquitto';
+
+var client = null;
+
+var isCommandSend = false;
+
 function setButtonsStatus(status) {
   $('#buttonEntrance').button(status);
   $('#buttonExit').button(status);
@@ -29,6 +37,8 @@ function sendCommand(userCommand) {
     //Success callback
     success: function(result) {
 
+      isCommandSend = true;
+
       setButtonsStatus('disable');
       setTimeout(function(){ setButtonsStatus('enable'); }, settingsDelay*1000);
 
@@ -42,6 +52,56 @@ function sendCommand(userCommand) {
       alert("Error: " + xhr.status + " " + xhr.statusText);
     }
   });
+}
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("/door");
+  client.subscribe("/barrier");
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+function getCurrentTime() {
+  return new Date().toString();
+}
+
+function changeIcon(linkId) {
+  if ($(linkId).hasClass('ui-icon-delete')) {
+    $(linkId).removeClass('ui-icon-delete');
+    $(linkId).addClass('ui-icon-check');
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  try {
+    if (false === isCommandSend) {
+      //ignore messages if command has not been sent
+      return;
+    }
+    var data = JSON.parse(message.payloadString);
+    if ('ok' === data.status) {
+      if ('/door' === message.destinationName) {
+        $('#statusDoor').text('opened at: ' + getCurrentTime());
+        changeIcon('#statusLinkDoor');
+      }
+      else if ('/barrier' === message.destinationName) {
+        $('#statusBarrier').text('opened at: ' + getCurrentTime());
+        changeIcon('#statusLinkBarrier');
+      }
+    }
+  }
+  catch(err) {
+      console.log("MQTT message: "+message.payloadString);
+  }
 }
 
 $(document).ready(function() {
@@ -62,4 +122,13 @@ $(document).ready(function() {
     sendCommand('door');
   });
 
+  // Create a client instance
+  client = new Paho.MQTT.Client(mqttHost, Number(mqttPort), mqttPath, "clientWeb");
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+
+  // connect the client
+  client.connect({onSuccess:onConnect});
 });
